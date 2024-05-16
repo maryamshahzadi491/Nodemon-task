ff2035ed48b1aa63ab0321157c050ce36bbbc886
maryamshahzadi491 <163430256+maryamshahzadi491@users.noreply.github.com>
2024-05-16 09:44:43 -0600

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,10 @@
+var studetns = ["Usman", "Ali", "Nouman"];
+//let searchstudents = studetns.findIndex(findAli);
+//studetns [5] = "Zeeshan" ;
+// studetns.sort();
+studetns. splice(2,1, ... ["Zahid", "Anwar"]);
+console. log(...studetns);
+function findAli(std){
+if(std == "'Ali") return true;
+else return false;
+}
---
+++ arrowfun.js
@@ -0,0 +1,6 @@
+/*let test =() => {
+    return "Ali";
+}
+console.log(test());*/           
+let test = ()=> "Ali";
+console.log(test());
